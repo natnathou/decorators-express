3a676c0d74fd7a7d39bf85402f5c5e6dc44ad037
netanel <ne.zaffran@gmail.com>
2023-08-07 23:28:52 +0300

    fix guard

--- src/controllers/decorators/guard.decorator.ts
+++ src/controllers/decorators/guard.decorator.ts
@@ -3,7 +3,7 @@ import { MetadataKey } from "../constants";
 import {Request, Response} from "express";
 
 export type GuardFncType = (req: Request, res: Response)=> boolean
-function Guard(guardFnc: GuardFncType){
+export function Guard(guardFnc: GuardFncType){
     return function (prototype: any, propertyKey: string, descriptor: PropertyDescriptor) {
         Reflect.defineMetadata(MetadataKey.middlewares , guardFnc, prototype, propertyKey);
     }
