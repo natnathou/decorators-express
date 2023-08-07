20dfb009d726b60638133c30f0aa9e8081ccdda0
netanel <ne.zaffran@gmail.com>
2023-08-07 23:38:29 +0300

    fix Use

--- src/controllers/decorators/use.decorator.ts
+++ src/controllers/decorators/use.decorator.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata';
 import { MetadataKey } from "../constants";
 import {RequestHandler} from "express-serve-static-core";
 
-function Use(middleware: RequestHandler){
+export function Use(middleware: RequestHandler){
     return function (prototype: any, propertyKey: string, descriptor: PropertyDescriptor) {
         const middlewares: RequestHandler[] = Reflect.getMetadata(MetadataKey.middlewares, prototype, propertyKey) || [];
         middlewares.push(middleware);
