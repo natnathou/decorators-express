60f66998e4f63d20e30db1cac1669f3b6dd9b725
netanel <ne.zaffran@gmail.com>
2023-12-06 22:06:29 +0200

    fix bug lint global middleware and guard

--- src/controllers/decorators/guard.decorator.ts
+++ src/controllers/decorators/guard.decorator.ts
@@ -5,7 +5,16 @@ import { MetadataKey } from '../constants';
 
 export type GuardFncType = (req: Request, res: Response)=> boolean;
 export function Guard(guardFnc: GuardFncType) {
-  return function (prototype: any, propertyKey: string, descriptor: PropertyDescriptor) {
-    Reflect.defineMetadata(MetadataKey.guard, guardFnc, prototype, propertyKey);
+  return function (target: any, propertyKey?: string, descriptor?: PropertyDescriptor) {
+    if (propertyKey && descriptor) {
+      const prototype = target;
+      Reflect.defineMetadata(MetadataKey.guard, guardFnc, prototype, propertyKey);
+    } else {
+      const constructor = target;
+      const prototypeKeys = Object.getOwnPropertyNames(constructor.prototype).filter((p) => p !== 'constructor');
+      for (const key of prototypeKeys) {
+        Reflect.defineMetadata(MetadataKey.guard, guardFnc, constructor.prototype, key);
+      }
+    }
   };
 }
--- src/controllers/decorators/use.decorator.ts
+++ src/controllers/decorators/use.decorator.ts
@@ -4,9 +4,20 @@ import type { RequestHandler } from 'express-serve-static-core';
 import { MetadataKey } from '../constants';
 
 export function Use(middleware: RequestHandler) {
-  return function (prototype: any, propertyKey: string, descriptor: PropertyDescriptor) {
-    const middlewares: RequestHandler[] = Reflect.getMetadata(MetadataKey.middlewares, prototype, propertyKey) || [];
-    middlewares.push(middleware);
-    Reflect.defineMetadata(MetadataKey.middlewares, middlewares, prototype, propertyKey);
+  return function (target: any, propertyKey?: string, descriptor?: PropertyDescriptor) {
+    if (propertyKey && descriptor) {
+      const middlewares: RequestHandler[] = Reflect.getMetadata(MetadataKey.middlewares, target, propertyKey) || [];
+      middlewares.push(middleware);
+      const prototype = target;
+      Reflect.defineMetadata(MetadataKey.middlewares, middlewares, prototype, propertyKey);
+    } else {
+      const constructor = target;
+      const prototypeKeys = Object.getOwnPropertyNames(constructor.prototype).filter((p) => p !== 'constructor');
+      for (const key of prototypeKeys) {
+        const middlewares: RequestHandler[] = Reflect.getMetadata(MetadataKey.middlewares, target, key) || [];
+        middlewares.push(middleware);
+        Reflect.defineMetadata(MetadataKey.middlewares, middlewares, constructor.prototype, key);
+      }
+    }
   };
 }
